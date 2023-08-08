ccd717d6e2e66a84cb138013d3ac176d8501f0cf
arnaldo16763695 <arnaldoespinoza1@hotmail.com>
2023-08-08 08:19:08 -0400

    send of email finished

--- script.js
+++ script.js
@@ -17,3 +17,41 @@
     $menu.classList.remove("is-active");
   });
 })(document);
+
+/****************** contact form  *******************/
+((d) => {
+  const $form = d.querySelector(".contact-form"),
+    $loader = d.querySelector(".contact-form-loader"),
+    $response = d.querySelector(".contact-form-response");
+
+  $form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    $loader.classList.remove("none");
+
+    fetch("https://formsubmit.co/ajax/arnaldoespinoza1@hotmail.com", {
+      method: "POST",
+      body: new FormData(e.target),
+    })
+      .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+      .then((json) => {
+        console.log(json);
+        $loader.classList.add("none");
+        location.hash = "thanks";
+        $form.reset();
+      })
+      .catch((err) => {
+        console.log(err);
+        let message =
+          err.statusText || "Ocurrió un error al enviar, intenta de nuevo";
+        $response.querySelector(
+          "h3"
+        ).innerHTML = `Error  ${err.statusText} : ${message}`;
+      })
+      .finally(() => {
+        $loader.classList.add("none");
+        setTimeout(() => {
+          location.hash = "#close";
+        }, 3000);
+      });
+  });
+})(document);
